42465b31072271a7ea2cdcdc82a0375a99d94e7b
prashnakhaling <prashna.krai@gmail.com>
2024-06-27 21:43:56 +0545

    added css

--- single-cpm_investor.php
+++ single-cpm_investor.php
@@ -12,22 +12,41 @@ get_header();
              <h2 class="investor_profile">CPM Investor Profile</h2>
              <p class="investor_moto">No.1 Investing Platform</p>
          </div>
-
+      </div>
          <!-- condition to check if the investor date is valid or not -->
          <?php
-         // get the data from the db
-
+          //assigning the 
           $investor_id = get_the_ID();
-          $publish_date_admin  = get_post_meta($investor_id, 'cpm_investor_publish_date', true);   //30th may
-          $valid_days = get_post_meta($investor_id, 'cpm_investor_valid_for', true);  
-       
+          $publish_date_admin  = get_post_meta($investor_id, 'cpm_investor_publish_date', true);   
+          $valid_days = get_post_meta($investor_id, 'cpm_investor_valid_for', true);  // get the data from the db
+  
          //  $total_valid_days = $publish_date_admin ." " .  $valid_days ;
          // echo $valid_days;
-          if( have_posts() ):
 
+          if( have_posts() ):?>
+   <div class="investor_content_div">
+           <?php
             if( $valid_days >= 0):
-               echo '<h2>' . get_the_title() . '</h2>';               
+            ?>
+               <div class="cpm_investor_logo">
+
+                       <?php echo '<p class= "investor_logo">' . get_the_post_thumbnail(); '</p>'; ?>
+
+                       <?php   echo '<h2 class="investor_username">' . get_the_title() . '</h2>';  ?>
+
+                        
+               </div>
+               <div>
+
+               </div>
+
+              
+               <?php
+               else :
+                  // If no posts found
+                  echo 'No posts found.';          
             endif;
+             
 
          endif;
           
@@ -38,6 +57,8 @@ get_header();
 
       
          ?>
-      </div>
+   </div>  
   </section>
+  <?php
+  get_footer();     
 
