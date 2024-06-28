2794d7aa7959ee717c16ace2b788f4d169953989
ranju58 <ranjubhusal01@gmail.com>
2024-06-28 11:59:36 +0545

    archive

--- archive-cpm_investor.php
+++ archive-cpm_investor.php
@@ -11,7 +11,7 @@ get_header();
         <?php
             // Start the Loop.
             while ( have_posts() ) :
-                the_post();
+                the_title();
             endwhile;
         endif;
         ?>
