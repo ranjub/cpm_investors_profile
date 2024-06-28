fb557ffea02bf9efc601acdc938c997d1223f9f6
prashnakhaling <prashna.krai@gmail.com>
2024-06-28 09:59:01 +0545

    Update archive-cpm_investor.php
    changes in archieve page

--- archive-cpm_investor.php
+++ archive-cpm_investor.php
@@ -4,11 +4,10 @@ get_header();
 
 <div class="content-area">
     <main class="site-main">
-        <?php if ( have_posts() ) : ?>
         <header class="page-header">
             <h1 class="page-title">Investors</h1>
         </header>
-
+        <?php if ( have_posts() ) : ?>
         <?php
             // Start the Loop.
             while ( have_posts() ) :
