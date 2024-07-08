dad0756f63c8785e3cbf962b3f1686de6b7ad6c3
ranju58 <ranjubhusal01@gmail.com>
2024-07-08 10:06:49 +0545

    archive page translation

--- templates/archive-cpm_investor.php
+++ templates/archive-cpm_investor.php
@@ -4,12 +4,14 @@ get_header();
 ?>
 
 <div class="investors-archive">
-    <h1>Investors</h1>
+    <h1><?php _e('Investors', 'cpm_investors'); ?></h1>
     <!-- Filter for search area -->
-    <h3>Search By</h3>
+    <h3><?php _e('Search By', 'cpm_investors'); ?></h3>
     <div class="filter-search">
         <form id="searchform" method="get">
-            <input type="text" id="searchFilter" name="searcharea" placeholder="Free text search" value="<?php echo get_search_query(), esc_attr($_GET['searcharea'] ?? ''); ?>" />
+            <input type="text" id="searchFilter" name="searcharea"
+                placeholder="<?php esc_attr_e('Free text search', 'cpm_investors'); ?>"
+                value="<?php echo get_search_query(), esc_attr($_GET['searcharea'] ?? ''); ?>" />
 
             <?php
             // Fetch unique countries
@@ -36,9 +38,10 @@ get_header();
             <select id="searchFilter" name="country">
                 <option value=""><?php esc_html_e('Select Country', 'cpm_investors'); ?></option>
                 <?php foreach ($unique_countries as $country) : ?>
-                    <option value="<?php echo esc_attr($country); ?>" <?php selected(esc_attr($_GET['country'] ?? ''), $country); ?>>
-                        <?php echo esc_html($country); ?>
-                    </option>
+                <option value="<?php echo esc_attr($country); ?>"
+                    <?php selected(esc_attr($_GET['country'] ?? ''), $country); ?>>
+                    <?php echo esc_html($country); ?>
+                </option>
                 <?php endforeach; ?>
             </select>
 
@@ -46,19 +49,22 @@ get_header();
                 <option value=""><?php esc_html_e('Select Investor Type', 'cpm_investors'); ?></option>
                 <option value="VC" <?php selected(esc_attr($_GET['searchinvestortype'] ?? ''), 'VC'); ?>>
                     <?php esc_html_e('VC', 'cpm_investors'); ?></option>
-                <option value="Accelerator" <?php selected(esc_attr($_GET['searchinvestortype'] ?? ''), 'Accelerator'); ?>>
+                <option value="Accelerator"
+                    <?php selected(esc_attr($_GET['searchinvestortype'] ?? ''), 'Accelerator'); ?>>
                     <?php esc_html_e('Accelerator', 'cpm_investors'); ?></option>
             </select>
 
             <select id="searchFilter" name="searchstatus">
                 <option value=""><?php esc_html_e('Select Investing Status', 'cpm_investors'); ?></option>
-                <option value="Actively Investing" <?php selected(esc_attr($_GET['searchstatus'] ?? ''), 'Actively Investing'); ?>>
+                <option value="Actively Investing"
+                    <?php selected(esc_attr($_GET['searchstatus'] ?? ''), 'Actively Investing'); ?>>
                     <?php esc_html_e('Actively Investing', 'cpm_investors'); ?></option>
-                <option value="Relaxed Investing" <?php selected(esc_attr($_GET['searchstatus'] ?? ''), 'Relaxed Investing'); ?>>
+                <option value="Relaxed Investing"
+                    <?php selected(esc_attr($_GET['searchstatus'] ?? ''), 'Relaxed Investing'); ?>>
                     <?php esc_html_e('Relaxed Investing', 'cpm_investors'); ?></option>
             </select>
 
-            <button type="submit">Filter</button>
+            <button type="submit"><?php esc_html_e('Filter', 'cpm_investors'); ?></button>
         </form>
     </div>
 
@@ -111,31 +117,31 @@ get_header();
                 $investing_status = get_post_meta($investor_id, 'cpm_investing_status', true); // Get the investing status
                 if ($valid_days >= 0) :
         ?>
-                    <div class="investor-item">
-                        <a href="<?php the_permalink(); ?>">
-                            <div class="investor-container">
-                                <?php if (has_post_thumbnail()) : ?>
-                                    <div class="investor-logo">
-                                        <?php the_post_thumbnail('medium'); ?>
-                                    </div>
-                                <?php endif; ?>
-                                <h2 class="investor-title">
-                                    <?php the_title();
-                                    if ($investing_status == "Actively Investing") { ?>
-                                        <i class="fa-solid fa-circle active-investing"></i>
-                                    <?php } else { ?>
-                                        <i class="fa-solid fa-circle relaxed-investing"></i>
-                                    <?php } ?>
-                                </h2>
-                            </div>
-                        </a>
+        <div class="investor-item">
+            <a href="<?php the_permalink(); ?>">
+                <div class="investor-container">
+                    <?php if (has_post_thumbnail()) : ?>
+                    <div class="investor-logo">
+                        <?php the_post_thumbnail('medium'); ?>
                     </div>
-            <?php
+                    <?php endif; ?>
+                    <h2 class="investor-title">
+                        <?php the_title();
+                                    if ($investing_status == "Actively Investing") { ?>
+                        <i class="fa-solid fa-circle active-investing"></i>
+                        <?php } else { ?>
+                        <i class="fa-solid fa-circle relaxed-investing"></i>
+                        <?php } ?>
+                    </h2>
+                </div>
+            </a>
+        </div>
+        <?php
                 endif;
             endwhile;
         else :
             ?>
-            <p><?php esc_html_e('No investors found.', 'cpm_investors'); ?></p>
+        <p><?php esc_html_e('No investors found.', 'cpm_investors'); ?></p>
         <?php
         endif;
         wp_reset_postdata();
